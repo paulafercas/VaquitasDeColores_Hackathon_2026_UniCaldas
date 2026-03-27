94de69fcecded65060dc9adbf2e39021497c2ee8
Paula Andrea Fernandez <pfernandezc@unal.edu.co>
2026-03-27 10:10:16 -0500

    Primeras lineas de codigo

--- Motor_analitico.py
+++ Motor_analitico.py
@@ -5,4 +5,14 @@ Importamos las librerías necesarias
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+from pandasql import sqldf
+
+#Dataframe para 1_Data_Recordings.csv
+df_1_Data_Recordings = pd.read_csv('1_Data_Recordings.csv')
+
+#Dataframe para 2_Data_Recordings.csv
+df_2_Data_Recordings = pd.read_csv('2_Data_Recordings.csv')
+
+#Ejecutamos sql
+pysqldf = lambda q: sqldf(q, globals())
 
